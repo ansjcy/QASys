937725c102b019d173795e6e480de9843f543659
Jason Chenyang Ji <chenyangjizju@gmail.com>
2017-12-11 00:04:50 -0500

    update

--- front/js/index.js
+++ front/js/index.js
@@ -1,27 +1,4 @@
 $(function () {
 
 
-
-
-    //BEGIN INTRO JS
-    $(window).load(function() {
-        //introJs().start();
-        //console.log("load window in here and emit request");
-
-        var socket = io.connect("http://localhost:1234");
-        socket.emit("aa", { my: 'data' });
-        socket.on('getData', function (data) {
-           console.log(data);
-           //socket.emit('my other event', { my: 'data' });
-        });
-
-
-        //var socket = io.connect('http://localhost');
-        //socket.on('news', function (data) {
-        //    console.log(data);
-        //    socket.emit('my other event', { my: 'data' });
-        //});
-
-    });
-    //END INTRO JS
 });
